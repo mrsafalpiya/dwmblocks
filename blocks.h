53410d9d76fd89de3b90fd0fdd610bf41b71d201
Safal Piya <66555275+mrsafalpiya@users.noreply.github.com>
2020-08-07 16:11:45 +0545

    Add files via upload

---
+++ blocks.h
@@ -0,0 +1,16 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
+	{"", 		"nettraf",		1,			0},
+
+	{"", 		"cpufreq",		2,			0},
+
+	{"", 		"cpubars",		2,			0},
+
+	{"",		"battery",		5,			0},
+
+	{"🗓", 		"clock",		60,			0},
+};
+
+//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+static char delim = ' ';
