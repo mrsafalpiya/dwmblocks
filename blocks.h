01889bcb06303d397beeccf37eda33f9f305ceb9
Safal Piya <66555275+mrsafalpiya@users.noreply.github.com>
2020-08-26 15:12:12 +0545

    Update blocks.h

--- blocks.h
+++ blocks.h
@@ -3,13 +3,13 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", 		"nettraf",		1,			0},
 
-	{"", 		"cpufreq",		2,			0},
+	{"|", 		"cpufreq",		2,			0},
 
 	{"", 		"cpubars",		2,			0},
 
-	{"",		"battery",		5,			0},
-
-	{"🗓", 		"clock",		60,			0},
+	{"|", 		"clock",		60,			0},
+	
+	{"|",		"battery",		5,			0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
